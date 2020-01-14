01004418d987f58b73ed387f8c470eb672076431
Vaidas <shibalba1@gmail.com>
2020-01-14 23:12:24 +0100

    add price highlight

--- src/App.js
+++ src/App.js
@@ -52,16 +52,16 @@ class App extends React.Component {
           }))
           .sort(
             firstBy((a, b) => 0)
-            .thenBy((a, b) => a.storage.type === 'SSD' ? -1 : (b.storage.type === 'SSD' ? 1 : 0))
             .thenBy((a, b) => parseFloat(a.cpu.freq.replace(' Ghz')) > parseFloat(b.cpu.freq.replace(' Ghz')))
             .thenBy((a, b) => a.ram.amout > b.ram.amout)
             .thenBy((a, b) => a.cpu.cores > b.cpu.cores)
+            .thenBy((a, b) => a.storage.type === 'SSD' ? -1 : (b.storage.type === 'SSD' ? 1 : 0))
+            .thenBy((a, b) => (a.storage.size * a.storage.amount * (a.storage.unit === 'TB' ? 1000 : 1)) > (b.storage.size * b.storage.amount * (b.storage.unit === 'TB' ? 1000 : 1)))
             .thenBy((a, b) => {
               const aBandw = getRealBandwidth(a.bandwidth);
               const bBandw = getRealBandwidth(b.bandwidth);
               return (aBandw.amount * (aBandw.unit === 'Gbps' ? 1024 : 1)) > (bBandw.amount * (bBandw.unit === 'Gbps' ? 1024 : 1));
             })
-            .thenBy((a, b) => (a.storage.size * a.storage.amount * (a.storage.unit === 'TB' ? 1000 : 1)) > (b.storage.size * b.storage.amount * (b.storage.unit === 'TB' ? 1000 : 1)))
             .thenBy((a, b) => a.ip > b.ip)
           )
       });
