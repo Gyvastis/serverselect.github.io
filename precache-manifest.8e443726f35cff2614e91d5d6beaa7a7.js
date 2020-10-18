4be39121ae52c328eaa9c0e01f3ea97824977946
Vaidas Bagdonas <vaidas.bagdonas@comsave.com>
2020-10-18 22:48:38 +0300

    Updates

---
+++ precache-manifest.8e443726f35cff2614e91d5d6beaa7a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8b635cb76f0dd0084e26466eff85f1",
+    "url": "/dedicated-servers/index.html"
+  },
+  {
+    "revision": "0ee61db004570e9c2557",
+    "url": "/dedicated-servers/static/css/main.a3673650.chunk.css"
+  },
+  {
+    "revision": "674e4f6122ba8d41436c",
+    "url": "/dedicated-servers/static/js/2.482e96f8.chunk.js"
+  },
+  {
+    "revision": "8f9a70a746088073fceab4989079aa3c",
+    "url": "/dedicated-servers/static/js/2.482e96f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ee61db004570e9c2557",
+    "url": "/dedicated-servers/static/js/main.a0812f37.chunk.js"
+  },
+  {
+    "revision": "543b57cb2b0dbe5ec829",
+    "url": "/dedicated-servers/static/js/runtime-main.2aad54b9.js"
+  }
+]);
